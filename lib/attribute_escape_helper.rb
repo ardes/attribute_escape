0a4b1a85042c237a5b680a9401be75b56159d965
Ian White <ian.w.white@gmail.com>
2008-12-05 14:49:39 +1100

    Make sure injection can't happen, replace & with 'and'

--- lib/attribute_escape_helper.rb
+++ lib/attribute_escape_helper.rb
@@ -1,6 +1,7 @@
 module AttributeEscapeHelper
+  ATTRIBUTE_ESCAPE = { '&' => 'and', '>' => '&gt;', '<' => '&lt;', '"' => "'" }
   def attribute_escape(s)
-    s.gsub('"', "'")
+    s.to_s.gsub(/[&"><]/) { |special| ATTRIBUTE_ESCAPE[special] }
   end
   alias_method :a, :attribute_escape
 end
--- spec/attribute_escape_helper_spec.rb
+++ spec/attribute_escape_helper_spec.rb
@@ -7,5 +7,13 @@ describe "AttributeEscapeHelper" do
     it 'should replace double quotes with single quotes' do
       attribute_escape('"G\'day"').should == "'G'day'"
     end
+    
+    it 'should replace ampersands with "and"' do
+      attribute_escape("Hello & Goodbye").should == 'Hello and Goodbye'
+    end
+    
+    it "should escape any dangerous html codes" do
+      attribute_escape("<foo />").should == '&lt;foo /&gt;'
+    end
   end
 end
